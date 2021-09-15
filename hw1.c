4fb2a32364ee503678bef10827f15877b1641d2a
HankyeolKim-SMU <201810927@sangmyung.kr>
2021-09-15 17:51:28 +0900

    homework

---
+++ hw1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	int num1, num2;
+	printf("두 정수를 입력하세요: ");
+	scanf_s("%d %d", &num1, &num2);
+	printf("%d & %d = %d\n", num1, num2, num1 & num2);
+	printf("%d | %d = %d\n", num1, num2, num1 | num2);
+	printf("%d ^ %d = %d\n", num1, num2, num1 ^ num2);
+}
